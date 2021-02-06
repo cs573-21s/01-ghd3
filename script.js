6a3ccb0609e4ca7639c209c9c636059acf2a1291
mastlouis <mastlouis@wpi.edu>
2021-02-05 19:26:40 -0500

    Chaotic shapes!

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ let svg = d3.select('#wind-chart')
   .attr('width', width)
   .attr('height', height);
 
+let slider = d3.select("#slider");
+// let slider = document.getElementById("slider");
+
+let chartInterval = null;
 
 function nextDay() {
   LOAD_DATA.then(data => {
@@ -30,6 +34,23 @@ function nextDay() {
   })
 }
 
+function setSlider(range) {
+  slider
+    // .attr("disabled", null) // Thanks https://stackoverflow.com/questions/15322556/how-to-remove-an-attribute-in-d3-js
+    .attr("max", range - 1)
+    .attr("value", 0);
+  // slider.setAttribute("max", range);
+  // slider.setAttribute("value", 0);
+  // slider.removeAttribute("disabled");
+}
+
+function placeSlider() {
+  selectedDay = slider.attr("value") // Thanks https://stackoverflow.com/questions/43646573/d3-get-attributes-from-element
+  displayForDay(selectedDay);
+  chartInterval.stop();
+  debugger;
+}
+
 const LOAD_DATA = new Promise((resolve, reject) => {
   Promise.all([
     d3.csv("50Hertz.csv"),
@@ -38,8 +59,9 @@ const LOAD_DATA = new Promise((resolve, reject) => {
     d3.csv("TransnetBW.csv"),
   ]).then(values => {
     // displayForDay(selectedDay);
-    d3.interval(nextDay, 500)
+    chartInterval = d3.interval(nextDay, 500);
     console.log('All data loaded!');
+    setSlider(values[0].length)
     resolve(values);
   }).catch(err => {
     console.error(`Error loading the csv data: ${err}`);
@@ -92,6 +114,8 @@ function displayForDay(day) {
         },
       ]);
       document.getElementById('data-date').innerHTML = `Showing data for ${data[0][day]["Date"]}`
+      slider.attr('value', selectedDay);
+      // slider.setAttribute("value", selectedDay)
   });
 }
 
@@ -124,9 +148,9 @@ function buildVis(dataArray){
     let line = d3.line()
       .defined(d => !isNaN(d.value))
       .x(d => x(d.date))
-      .y(d => y(d.value))
+      .y(d => y(d.value));
 
-      svg.append("path")
+    svg.append("path")
       .datum(companyData)
       .attr("fill", "none")
       .attr("stroke", color)
@@ -137,6 +161,52 @@ function buildVis(dataArray){
       .attr("d", line);
   }
 
+  function addCircles(name, companyData, color) {
+    svg.append("g")
+      .selectAll("dot")
+      .data(companyData)
+      .enter()
+      .append("circle")
+        .attr("cx", d => x(d.date))
+        .attr("cy", d => y(d.value))
+        .attr("r", 2)
+        .attr("fill", color)
+        .attr("company-name", name)
+  }
+
+  function addSymbols(name, companyData, color){
+    svg.append("g").selectAll('path')
+    .data(companyData)
+    .enter()
+    .append("path")
+      .attr("class", "symbol-point")
+      // .attr("d", d3.symbol().type(d3.symbolTriangle)())
+      .attr("d", d3.symbol().type(getRandomSymbol())())
+      .attr("transform", d => `translate(${x(d.date)},${y(d.value)})`) 
+      .style("fill", color)
+      .style("opacity", 0.4)
+      .attr("company-name", name)
+  }
+
+  function addFill(name, companyData, color){
+    svg.append("g").selectAll("path")
+    .data(companyData)
+    .enter()
+    .append("path")
+      .attr("d", d3.area()
+        .x(d => x(d.date))
+        .y0(d => height - margin.bottom)
+        .y1(d => y(d.value))(companyData)
+      )
+      // .attr("points", d => d.map( d=> [x(d.time), y(d.value)].join(",")).join(" "))
+      // .attr("stroke", "black")
+      .attr("fill", color)
+      // .attr("stroke-width", 2)
+      .attr("fill-opacity", 0.01)
+      .attr("company-name", name)
+      // .attr("stroke-opacity", 0.5)
+  }
+
   svg.append("g")
     .call(xAxis);
 
@@ -144,7 +214,53 @@ function buildVis(dataArray){
     .call(yAxis);
 
   for (let dataset of dataArray) {
-    addLine(dataset.name, dataset.points, dataset.color);
+    let displayState = Math.floor(Math.random() * 4);
+    switch (displayState) {
+      case 0:
+        addLine(dataset.name, dataset.points, dataset.color);
+        break;
+      case 1:
+        addCircles(dataset.name, dataset.points, dataset.color);
+        break;
+      case 2:
+        addSymbols(dataset.name, dataset.points, dataset.color);
+        break;
+      case 3:
+        addFill(dataset.name, dataset.points, dataset.color);
+        break;
+    }
   }
+
+  // svg.append("g").selectAll("path")
+  //   .data(dataArray[0].points)
+  //   .enter()
+  //   .append("path")
+  //     .attr("d", d3.area()
+  //       .x(d => x(d.date))
+  //       .y0(d => 0)
+  //       .y1(d => y(d.value))(dataArray[0].points)
+  //     )
+  //     // .attr("points", d => d.map( d=> [x(d.time), y(d.value)].join(",")).join(" "))
+  //     // .attr("stroke", "black")
+  //     .attr("fill", "green")
+  //     // .attr("stroke-width", 2)
+  //     .attr("fill-opacity", 0.005)
+  //     // .attr("stroke-opacity", 0.5)
+
   console.log('Line should be drawn now.');
+}
+
+function getRandomSymbol() {
+  let symbols = [
+    d3.symbolCircle, 
+    d3.symbolCross, 
+    d3.symbolDiamond, 
+    d3.symbolSquare, 
+    d3.symbolStar, 
+    d3.symbolTriangle, 
+    d3.symbolWye
+  ];
+
+  return symbols[Math.floor(Math.random() * symbols.length)];
+
 }
